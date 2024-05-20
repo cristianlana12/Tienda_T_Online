1d600a4cfa87ef07d328912ac46e9799772fde8a
Cristian Cordoba <cristian56cordoba@gmail.com>
2024-05-20 11:39:09 -0300

    Listo el autoLoad

--- autoload.php
+++ autoload.php
@@ -2,5 +2,9 @@
     spl_autoload_register(function ($class_name) {
         //OBTENEMOS TODA LA RUTA DEL ARCHIVO QUE USAREMOS
         $archivo = __DIR__ . "/" . $class_name . ".php"; //en la variable archivo guardamos la ruta del archivo de la clase, donde asigmamos el valor de la constante __DIR__ esta constante optiene el directorio actual que se esta ejecutando
-        
+        $archivo = str_replace("\\", "/", $archivo);
+
+        if (is_file($archivo)) {
+            require_once($archivo);
+        }
     });
