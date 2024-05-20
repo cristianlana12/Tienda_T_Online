1d87b3dba34f130025c814bed8c5be6ec272282a
Cristian Cordoba <cristian56cordoba@gmail.com>
2024-05-20 20:35:48 -0300

    creacion de los archivos del head y sessionStart

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
 <?php
-    require_once("./config/app.php");
-    require_once("./autoload.php");
+require_once ("./config/app.php");
+require_once ("./autoload.php");
+require_once ("./app/views/inc/session_start.php");
+
+if (isset($_GET['views'])) {
+    $url = explode("/", $_GET['views']);   //aqui partiremos la url
+} else {
+    $url = ["login"];
+}
+
 
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <?php require_once ("./app/views/inc/head.php"); ?>
+</head>
+
+<body>
+    <?php require_once ("./app/views/inc/script.php"); ?>
+
+</body>
+
+</html>
