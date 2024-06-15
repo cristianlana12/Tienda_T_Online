0c2e872257a80b8ca989ef8e26190ad9a1020803
Cristian Cordoba <cristian56cordoba@gmail.com>
2024-06-15 18:58:36 -0300

    Creando el modelo principal con las variables de la clase mainModel

---
+++ app/models/mainModel.php
@@ -0,0 +1,15 @@
+<?php
+    namespace App\Models;
+
+    if (file_exists(__DIR__."/../../config/server.php")) {
+        require_once(__DIR__."/../../config/server.php");
+    }
+    class mainModel 
+    {
+        private $server = DB_SERVER;
+        private $db = DB_NAME;
+        private $user = DB_USER;
+        private $pass = DB_PASS;
+    }
+    
+?>
