f586e3ca82e798d3cf3373c909093ce59a3b9e87
Cristian Cordoba <cristian56cordoba@gmail.com>
2024-06-15 19:20:06 -0300

    Creacion de funcion para conectar a la base de datos

--- app/models/mainModel.php
+++ app/models/mainModel.php
@@ -1,15 +1,23 @@
 <?php
-    namespace App\Models;
+namespace App\Models;
+use \PDO;
 
-    if (file_exists(__DIR__."/../../config/server.php")) {
-        require_once(__DIR__."/../../config/server.php");
-    }
-    class mainModel 
+if (file_exists(__DIR__ . "/../../config/server.php")) {
+    require_once (__DIR__ . "/../../config/server.php");
+}
+class mainModel
+{
+    private $server = DB_SERVER;
+    private $db = DB_NAME;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+
+    protected function conectar()
     {
-        private $server = DB_SERVER;
-        private $db = DB_NAME;
-        private $user = DB_USER;
-        private $pass = DB_PASS;
+        $conexion = new PDO("mysql:host=" . $this->server . ";dbname=" . $this->db, $this->user, $this->pass);
+        $conexion->exec("SET CHARACTER SET utf8");
+        return $conexion;
     }
-    
+}
+
 ?>
