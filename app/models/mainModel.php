af45953311b8d0d91525a0b5c4a4ef6fcf825ac2
Cristian Cordoba <cristian56cordoba@gmail.com>
2024-06-26 11:47:36 -0300

    incorporar funcion para ejecutar consultas SQL

--- app/models/mainModel.php
+++ app/models/mainModel.php
@@ -1,10 +1,13 @@
 <?php
 namespace App\Models;
+
 use \PDO;
 
 if (file_exists(__DIR__ . "/../../config/server.php")) {
     require_once (__DIR__ . "/../../config/server.php");
 }
+
+//MODELO PRINCIPAL 
 class mainModel
 {
     private $server = DB_SERVER;
@@ -17,6 +20,14 @@ class mainModel
         $conexion = new PDO("mysql:host=" . $this->server . ";dbname=" . $this->db, $this->user, $this->pass);
         $conexion->exec("SET CHARACTER SET utf8");
         return $conexion;
+    }
+
+    //MODELO o FUNCION: Que nos permite ejecutar consultas
+    protected function ejecutarConsulta($consulta)
+    { //funcion que recibe un parametro, que sera la consulta a ejecutar
+        $sql = $this->conectar()->prepare($consulta); //accedo a la funcion conectar de la clase y llamo al metodo prepare() para preparar una consulta
+        $sql->execute(); //Con esto ejecutamos la consulta
+        return $sql;
     }
 }
 
